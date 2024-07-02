29cffb6b11736dc75a313623c11d0b7ec2c69f90
khushbii683 <btech10351.19@bitmesra.ac.in>
2024-07-03 00:00:03 +0530

    added local mongodb

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL, {
+    await mongoose.connect(process.env.DATABASE_URL ||'mongodb://root:example@localhost:27017', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
